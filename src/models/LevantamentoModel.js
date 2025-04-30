09a149f9b6b27debf4c2faab959e34fa1d623fb2
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 16:02:53 -0400

    model do levantamento

---
+++ src/models/LevantamentoModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+import { type } from "os";
+
+class LevantamentoModel {
+    constructor() {}
+    const = levantamentoSchema = new mongoose.Schema({
+        imagem: {
+            type: String,
+        },
+        estado: {
+            type: String,
+            required: true,
+        },
+        data: {
+            type: Date,
+            required: true,
+        }
+    })
+   
+}
+

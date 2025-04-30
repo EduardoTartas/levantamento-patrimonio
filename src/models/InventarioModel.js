da6b787e7b5b7f45511f59ed73d23d62d078b54c
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-30 15:53:11 -0400

    model do inventario

---
+++ src/models/InventarioModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+import { type } from "os";
+
+class InventarioModel {
+    constructor() {}
+    const = inventarioSchema = new mongoose.Schema({
+        nome: {
+            type: String,
+            index: true,
+            required: true,
+        },
+        data: {
+            type: Date,
+            required: true,
+        },
+        status: {
+            type: Boolean,
+            default: "false",
+        },
+    })
+   
+}

92bbc313e79a30f6d97d7aad8bc8794a4952b886
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-05-07 16:42:34 -0400

    levantamento seeds

---
+++ src/seeds/levantamentoSeed.js
@@ -0,0 +1,45 @@
+import fakerbr from 'faker-br';
+import Inventario from '../models/inventario.js';
+import Sala from '../models/Sala.js';
+import Usuario from '../models/Usuario.js';
+import Levantamento from '../models/Levantamento.js';
+import Bem from '../models/Bem.js';
+
+export default async function levantamentoSeed() {
+    // Coleta os inventários, salas, usuários e bens cadastrados no banco de dados
+    const inventarioList = await Inventario.find({});
+    const salaList = await Sala.find({});
+    const usuarioList = await Usuario.find({});
+    const bemList = await Bem.find({});
+
+    // Deleta todos os levantamentos existentes no banco de dados
+    await Levantamento.deleteMany({});
+
+    // Gera 100 levantamentos
+    for (let i = 0; i < 100; i++) {
+        const randomInventario = inventarioList[Math.floor(Math.random() * inventarioList.length)];
+        const randomSala = salaList[Math.floor(Math.random() * salaList.length)];
+        const randomUsuario = usuarioList[Math.floor(Math.random() * usuarioList.length)];
+        const randomBem = bemList[Math.floor(Math.random() * bemList.length)];
+
+        const levantamento = {
+            inventario: randomInventario._id,
+            bem: {
+                salaID: randomBem.salaID || randomSala._id,
+                nome: randomBem.nome || fakerbr.commerce.productName(),
+                tombo: randomBem.tombo || fakebr.random.alphaNumeric(10),
+                responsavel: randomBem.responsavel || fakerbr.name.findName(),
+                ocioso: randomBem.ocioso || fakerbr.random.boolean(),
+            },
+            sala: randomSala._id,
+            usuario: randomUsuario._id,
+            imagem: fakerbr.image.imageUrl(),
+            estado: fakerbr.random.arrayElement(["Em condições de uso", "Inservível", "Danificado"]),
+            data: fakerbr.date.past(),
+        };
+
+        await Levantamento.create(levantamento);
+    }
+
+    console.log("Levantamentos gerados com sucesso");
+}

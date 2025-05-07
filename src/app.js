271e267034ee601ccbfd8f6b2830529c6144d57c
Eduardo dos Santos Tartas <2024103070003@aluno.ifro.edu.br>
2025-05-06 20:23:22 -0400

    fix

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+/*import routes from "./routes/index.js";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import DbConect from './config/DbConnect.js';
+import errorHandler from './utils/helpers/errorHandler.js';
+import logger from './utils/logger.js';
+import CommonResponse from './utils/helpers/CommonResponse.js';
+import fileUpload from 'express-fileupload';*/
+
+const app = express();
+
+
+export default app;

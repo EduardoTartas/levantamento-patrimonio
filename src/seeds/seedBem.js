9d98aa6cde761a019e3dc7aed9be6a3114f167f9
Thiago Suchi <thiagohenssuchi@gmail.com>
2025-05-06 22:18:36 -0400

    Arquivo seed dos bens implementado...

--- src/seeds/seedBem.js
+++ src/seeds/seedBem.js
@@ -4,13 +4,8 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
 import DbConnect from '../config/dbConnect';
-import UsuarioModel from '../models/Usuario';
+import CampusModel from '../models/Campus';
 
 import getGlobalFakeMapping from './globalFakeMapping';
 
-export function gerarSenhaHash(senha) {
-    return bcrypt.hashSync(senha, 10);
-}
-
-let senhaPura = "Abc@321";
-let senhaHash = gerarSenhaHash(senhaPura);
+await DbConnect.conectar();

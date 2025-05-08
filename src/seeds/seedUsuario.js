8838da2ad0820ca236829ef8e28022bb4b654df8
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-07 22:33:12 -0400

    seedSala e seedUsuario estão sendo criados...

--- src/seeds/seedUsuario.js
+++ src/seeds/seedUsuario.js
@@ -7,10 +7,3 @@ import DbConnect from '../config/dbConnect';
 import UsuarioModel from '../models/Usuario';
 
 import getGlobalFakeMapping from './globalFakeMapping';
-
-export function gerarSenhaHash(senha) {
-    return bcrypt.hashSync(senha, 10);
-}
-
-let senhaPura = "Abc@321";
-let senhaHash = gerarSenhaHash(senhaPura);

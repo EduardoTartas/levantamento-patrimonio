50b6d259a33bc2a574fa09613ebd211955c8a4ff
EduardoTartas <eduardos.tartas@gmail.com>
2025-05-07 21:16:21 -0400

    seed campus arumada

--- src/seeds/seedCampus.js
+++ src/seeds/seedCampus.js
@@ -3,22 +3,25 @@ import Campus from "../models/Campus.js";
 import DbConnect from "../config/dbConnect.js";
 
 export default async function campusSeed() {
-  for (let i = 0; i < 10; i++) {
-    const campus = {
-      nome: fakerbr.lorem.word(10),
-      telefone: fakerbr.phone.phoneNumber().ToString(),
-      cidade: fakerbr.address.city(),
-      bairro: fakerbr.address.neighborhood(),
-      rua: fakerbr.address.streetName(),
-      numeroResidencia: fakerbr.address.streetAddress().ToString(),
-    };
-    await Campus.create(campus);
-  }
-  console.log("Campus seed completed");
+    // Deleta todos os campus existentes no banco de dados
+    await Campus.deleteMany({});
+
+    // Gera 25 campus
+    for (let i = 0; i < 25; i++) {
+        const campus = {
+            nome: fakerbr.lorem.word(10),
+            telefone: fakerbr.phone.phoneNumber().toString(),
+            cidade: fakerbr.address.city(),
+            bairro: fakerbr.lorem.word(15),
+            rua: fakerbr.address.streetName().toString(),
+            numeroResidencia: fakerbr.address.streetAddress().toString(),
+        };
+
+        await Campus.create(campus);
+    }
+
+    console.log("Campus gerados com sucesso");
 }
 
-
-DbConnect.conectar();
-console.log("Conectado ao banco de dados");
-campusSeed();
-console.log("Seed executado com sucesso");
+await DbConnect.conectar();
+await campusSeed();

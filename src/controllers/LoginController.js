310600420d4810d45f35ac66a7e0401a3a5547c0
ThiagoHS <thiagohenssuchi@gmail.com>
2025-05-09 17:41:46 -0400

    Criação do repostório controller, implementação de LoginController e UsuarioController...

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,40 @@
+// src/controllers/LoginController.js
+import Usuario from '../models/Usuario.js';
+import AuthenticationError from '../utils/errors/AuthenticationError.js';
+import { LoginSchema } from '../utils/validators/schemas/zod/LoginSchema.js';
+
+class LoginController {
+    // constructor() {
+    //     // Chamará o service    
+    // }
+
+    validarDados() {
+        // Valida os dados enviados no corpo da requisição usando o esquema LoginSchema
+        const resultado = LoginSchema.safeparse(req.body);
+
+        if (!resultado.success) {
+            // Captura o primeiro erro de validação e cria um erro de autenticação
+            const erro = resultado.error.errors[0];
+            // Aqui está passando o erro para o middleware de tratamento de erros
+            return next(new AuthenticationError(erro.message));
+        }
+    }
+
+    // POST /login
+    async login(req, res, next) {
+        this.validarDados(req.body);
+
+        const { email, senha } = req.body;
+
+        // Busca no banco de dados um usuário com o email fornecido, incluindo o campo 'senha'
+        const usuario = await Usuario.findOne({ email }).select('+senha');
+
+        if (!usuario || usuario.senha !== senha) {
+            return next(new AuthenticationError('Email ou senha inválidos'));
+        }
+
+        res.status(200).json({ mensagem: 'Login realizado com sucesso.' });
+    }
+}
+
+export default LoginController;
---
+++ src/controllers/UsuarioController.js
@@ -0,0 +1,96 @@
+// src/controllers/UsuarioController.js
+import { UsuarioQuerySchema, UsuarioIdSchema } from '../utils/validators/schemas/zod/querys/UsuarioQuerySchema.js';
+import { UsuarioSchema, UsuarioUpdateSchema } from '../utils/validators/schemas/zod/UsuarioSchema.js';
+import {
+    CommonResponse,
+    CustomError,
+    HttpStatusCodes,
+} from '../utils/helpers/index.js';
+
+
+class UsuarioController {
+    // constructor() {
+         // Chama o código service
+    // }
+
+    // Lista usuários. Se um ID é fornecido, retorna um único objeto.
+    // Caso contrário, retorna todos os objetos com suporte a filtros e paginação.
+    async listar(req, res) {
+        console.log('Estou no listar em UsuarioController');
+
+        // Validação do ID (se existir)
+        const { id } = req.params || {};
+        if (id) {
+            UsuarioIdSchema.parse(id);
+        }
+
+        // Validação das queries (se existirem)
+        const query = req.query || {};
+        if (Object.keys(query).length !== 0) {
+            // deve apenas validar o objeto query, tendo erro o zod será responsável por lançar o erro
+            await UsuarioQuerySchema.parseAsync(query);
+        }
+
+        const data = await this.service.listar(req);
+        return CommonResponse.success(res, data);
+    }
+
+    // Cria um novo usuário.
+    async criar(req, res) {
+        console.log('Estou no criar em UsuarioController');
+
+        // Cria o DTO de criação e valida os dados
+        const parsedData = UsuarioSchema.parse(req.body);
+        let data = await this.service.criar(req);
+
+        // Converte o documento Mongoose para um objeto simples
+        let usuarioLimpo = data.toObject();
+
+        // Remove campos indesejados, como a senha e outros que não devem ser expostos
+        delete usuarioLimpo.senha;
+
+        return CommonResponse.created(res, usuarioLimpo);
+    }
+
+    // Atualiza um usuário existente.
+    async atualizar(req, res) {
+        console.log('Estou no atualizar em UsuarioController');
+
+        // destruturação do id
+        const { id } = req.params;
+        UsuarioIdSchema.parse(id);
+
+        const parsedData = UsuarioUpdateSchema.parse(req.body);
+       
+        const data = await this.service.atualizar(id, parsedData);
+
+        // Converte o documento Mongoose para um objeto simples
+        let usuarioLimpo = data.toObject();
+
+        // Remove campos indesejados, como a senha e outros que não devem ser expostos
+        delete usuarioLimpo.senha;
+
+        return CommonResponse.success(res, data, 200, 'Usuário atualizado com sucesso. Porém, o e-mail é ignorado em tentativas de atualização, pois é opração proibida. IDs de Unidades e Grupos não cadastradas são ignoradas.');
+    }
+
+    // Deleta um usuário existente.
+    async deletar(req, res) {
+        console.log('Estou no deletar em UsuarioController');
+
+        const { id } = req.params || {};
+        if (!id) {
+            throw new CustomError({
+                statusCode: HttpStatusCodes.BAD_REQUEST.code,
+                errorType: 'validationError',
+                field: 'id',
+                details: [],
+                customMessage: 'ID do usuário é obrigatório para deletar.'
+            });
+        }
+
+        const data = await this.service.deletar(id);
+        return CommonResponse.success(res, data, 200, 'Usuário excluído com sucesso.');
+    };
+}
+
+export default UsuarioController;

72040a5b54fcaf08a5e3bbbb93e6a5fabdab3cd8
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-05-10 11:10:26 -0400

    rota relatorio

---
+++ routes/relatorioRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+//import RelatorioController from "../controllers/RelatorioController.js";
+//import {asyncWrapper} from '../utils/helpers/index.js';
+//import AuthMiddleware from "../middlewares/AuthMiddleware.js";
+//import authPermission from '../middlewares/AuthPermission.js';
+
+const router = express.Router();
+
+//const relatorioController = new RelatorioController();
+
+router
+// Espera os parâmetros de query: inventarioId (obrigatório), sala (opcional), tipoRelatorio (obrigatório)
+.get("/relatorios",AuthMiddleware,authPermission,asyncWrapper(relatorioController.gerarRelatorio.bind(relatorioController)));
+
+export default router;

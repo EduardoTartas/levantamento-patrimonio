c115685960dc133c62fff10c08cd14ccd206543e
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-05-15 18:13:10 -0400

    adiciona CampusFilterBuilder para construção de filtros de campus

---
+++ src/repositories/filters/CampusFilterBuilder.js
@@ -0,0 +1,39 @@
+import Campus from '../../models/Campus.js';
+import CampusRepository from '../CampusRepository.js';
+
+class CampusFilterBuilder {
+    constructor() {
+        this.filtros = {};
+        this.campusRepository = new CampusRepository();
+        this.campusModel = Campus;
+    }
+
+    comNome(nome) {
+        if (nome) {
+            this.filtros.nome = { $regex: nome, $options: 'i' };
+        }
+        return this;
+    }
+
+    comCidade(cidade) {
+        if (cidade) {
+            this.filtros.cidade = { $regex: cidade, $options: 'i' };
+        }
+        return this;
+    }
+
+    comAtivo(ativo) {
+        if (ativo === 'true') {
+            this.filtros.status = true;
+        } else if (ativo === 'false') {
+            this.filtros.status = false;
+        }
+        return this;
+    }
+
+    build() {
+        return this.filtros;
+    }
+}
+
+export default CampusFilterBuilder;

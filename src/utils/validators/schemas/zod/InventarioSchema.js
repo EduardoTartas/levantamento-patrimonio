bf7b29491368480387c63de96af06029928a82fc
EduardoTartas <eduardos.tartas@gmail.com>
2025-05-20 19:46:20 -0400

    refatora validação de data no InventarioSchema para usar superRefine e melhorar a mensagem de erro

--- src/utils/validators/schemas/zod/InventarioSchema.js
+++ src/utils/validators/schemas/zod/InventarioSchema.js
@@ -1,25 +1,36 @@
-import { z } from 'zod';
+import {z} from 'zod';
 import objectIdSchema from "./ObjectIdSchema.js";
 
 const InventarioSchema = z.object({
     campus: objectIdSchema,
     nome: z.string().min(1, "Campo nome é obrigatório."),
     data: z.string()
-        .refine(
-            (val) => /^\d{2}\/\d{2}\/\d{4}$/.test(val),
-            { message: "Data deve estar no formato dd/mm/aaaa" }
-        )
-        .transform((val) => {
+        .superRefine((val, ctx) => {
+            if (!/^\d{2}\/\d{2}\/\d{4}$/.test(val)) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Data deve estar no formato dd/mm/aaaa"
+                });
+                return;
+            }
             const [dia, mes, ano] = val.split('/');
             const date = new Date(`${ano}-${mes}-${dia}T00:00:00`);
-            return date;
+            if (isNaN(date.getTime())) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Por favor, insira uma data válida no formato dd/mm/aaaa"
+                });
+            }
         })
-        .refine(
-            (date) => !isNaN(date.getTime()),
-            { message: "Por favor, insira uma data válida no formato dd/mm/aaaa" }
-        ),
+        .transform((val) => {
+            const [dia, mes, ano] = val.split('/');
+            return new Date(`${ano}-${mes}-${dia}T00:00:00`);
+        }),
     status: z.boolean().default(true)
 });
 const InventarioUpdateSchema = InventarioSchema.partial().extend({});
 
-export {InventarioSchema, InventarioUpdateSchema};
+export {
+    InventarioSchema,
+    InventarioUpdateSchema
+};

114e93d01f826f6d8dc1a07604d207aed916bda9
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-05-22 16:59:56 -0400

    Adicionar arquivo de setup do Jest para mock do console e atualizar o caminho da configuração

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,3 @@
-lobal.console = {
-  ...console,
-  // Manter erros visíveis
-  error: jest.fn(),
-  // Suprimir logs de informação
-  log: jest.fn(),
-  // Suprimir logs de aviso
-  warn: jest.fn(),
-  // Manter logs de informação
-  info: jest.fn()
-};
-
 beforeAll(() => {
     jest.spyOn(console, 'error').mockImplementation(() => { });
     jest.spyOn(console, 'log').mockImplementation(() => { });

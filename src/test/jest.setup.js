2f635c9dc45db4b8b3c7ec06ed5fb9cb7c116c94
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-05-22 15:28:26 -0400

    adiciona configuração para mock de console no Jest

--- src/test/jest.setup.js
+++ src/test/jest.setup.js
@@ -9,3 +9,17 @@ lobal.console = {
   // Manter logs de informação
   info: jest.fn()
 };
+
+beforeAll(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => { });
+    jest.spyOn(console, 'log').mockImplementation(() => { });
+});
+
+afterAll(() => {
+    if (console.error.mockRestore) {
+        console.error.mockRestore();
+    }
+    if (console.log.mockRestore) {
+        console.log.mockRestore();
+    }
+});

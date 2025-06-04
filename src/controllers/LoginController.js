2db9264b3dce2c6255b755de7c0886bbaefa2b51
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-04 17:53:26 -0400

    Refatoração do LoginController - lógica principal implementada e readaptada ao LoginService...

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,29 +1,26 @@
 // src/controllers/LoginController.js
-import LoginService from '../services/LoginService.js'
-import AuthenticationError from '../utils/errors/AuthenticationError.js';
-import { LoginSchema } from '../utils/validators/schemas/zod/LoginSchema.js';
+import { LoginRepository } from '../repositories/LoginRepository.js';
+import { LoginService } from '../services/LoginService.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 class LoginController {
     constructor() {
-        this.service = new LoginService();
+        this.repository = new LoginRepository;
+        this.service = new LoginService(process.env.JWT_SECRET, process.env.JWT_EXPIRES_IN, this.repository);
     }
 
     // POST /login
     async login(req, res, next) {
-        // Valida os dados enviados no corpo da requisição usando o esquema LoginSchema
-        const result = LoginSchema.safeParse(req.body);
-
-        if (!result.success) {
-            // Captura o primeiro erro de validação e cria um erro de autenticação
-            const error = result.error.errors[0];
-            // Aqui está passando o erro para o middleware de tratamento de erros
-            return next(new AuthenticationError(error.message));
-        }
-
         const { email, senha } = req.body;
 
-        const resultLogin = await this.service.autenticar(email, senha);
-        return res.status(200).json(resultLogin);
+        const usuario = await this.service.autenticar(email, senha);
+
+        res.status(200).json({
+            mensagem: 'Login realizado com sucesso.',
+            ...usuario
+        });
     }
 }
 

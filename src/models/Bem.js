6fd26416155439ab6b887931b9e9202d7c57884c
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-06-06 16:55:08 -0400

    refatora a estrutura do responsável para ser um objeto com nome e CPF

--- src/models/Bem.js
+++ src/models/Bem.js
@@ -1,53 +1,59 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
-class Bem{
-    constructor() {
-        const bemSchema = new mongoose.Schema({
-            sala: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "sala",
-                required: true,
-            },
-            nome: {
-                type: String,
-                index: true,
-                required: true,
-            },            
-            tombo: {
-                type: String,
-                required: false,
-                unique: function() { return this.tombo && this.tombo.trim() !== ''; },
-                sparse: true
-            },
-            responsavel: {
-                type: String,
-                required: true,
-            },
-            descricao: {
-                type: String,
-            },
-            valor: {
-                type: Number,
-                required: true,
-            },
-            auditado: {
-                type: Boolean,
-                default: false,
-            },
-            ocioso: {
-                type: Boolean,
-                default: false,
-            }
-        }, {
-            timestamps: true,
-            versionKey: false
-        });
-
-        bemSchema.plugin(mongoosePaginate);
-
-        this.model = mongoose.model('ben', bemSchema);
+const bemSchema = new mongoose.Schema({
+    sala: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "sala",
+        required: true,
+    },
+    nome: {
+        type: String,
+        index: true,
+        required: true,
+    },
+    tombo: {
+        type: String,
+        required: false,
+        unique: function() { return this.tombo && this.tombo.trim() !== ''; },
+        sparse: true
+    },
+    // --- ALTERAÇÃO AQUI ---
+    // O responsável agora é um objeto
+    responsavel: {
+        nome: { 
+            type: String, 
+            required: true 
+        },
+        cpf: { 
+            type: String, 
+            required: true,
+            index: true // Adicionar um índice ao CPF pode ser útil para buscas
+        }
+    },
+    // ----------------------
+    descricao: {
+        type: String,
+    },
+    valor: {
+        type: Number,
+        required: true,
+    },
+    auditado: {
+        type: Boolean,
+        default: false,
+    },
+    ocioso: {
+        type: Boolean,
+        default: false,
     }
-}
+}, {
+    timestamps: true,
+    versionKey: false
+});
 
-export default new Bem().model;
+bemSchema.plugin(mongoosePaginate);
+
+const Bem = mongoose.model('ben', bemSchema);
+
+export default Bem;

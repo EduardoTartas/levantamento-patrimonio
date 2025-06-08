56750e83096a05969e0717e8176ff1ae309702b2
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-06-08 17:42:51 -0400

    refatora BemController para corrigir a validação de ID e query

--- src/controllers/BemController.js
+++ src/controllers/BemController.js
@@ -1,7 +1,6 @@
 import BemService from '../services/BemService.js';
 import { CommonResponse, CustomError, HttpStatusCodes} from '../utils/helpers/index.js';
 import { BemQuerySchema, BemIdSchema } from '../utils/validators/schemas/zod/querys/BemQuerySchema.js';
-import { BemSchema} from '../utils/validators/schemas/zod/BemSchema.js';
 
 class BemController {
     constructor() {
@@ -10,15 +9,14 @@ class BemController {
 
     async listar(req, res) {
         console.log('Estou no listar em BemController, enviando req para BemService');
-
         const { id } = req.params || {};
         if (id) {
-            CampusIdSchema.parse(id);
+            BemIdSchema.parse(id);
         }
 
         const query = req.query || {};
         if (Object.keys(query).length !== 0) {
-            await CampusQuerySchema.parseAsync(query);
+            await BemQuerySchema.parseAsync(query);
         }
 
         const data = await this.service.listar(req);

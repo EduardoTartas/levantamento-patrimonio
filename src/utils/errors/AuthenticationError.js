85449812bb6affbeafcc4e74908dd8da86b493ad
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-12 16:33:20 -0400

    Funções refatoradas para serem implementadas com o CustomError...

--- src/utils/errors/AuthenticationError.js
+++ src/utils/errors/AuthenticationError.js
@@ -1,12 +1,17 @@
+import CustomError  from "../helpers/CustomError.js";
+
 // src/utils/errors/AuthenticationError.js
-class AuthenticationError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = 'AuthenticationError';
-      this.statusCode = 498;
-      this.isOperational = true;
-    }
+class AuthenticationError extends CustomError {
+  constructor(message, field = null, details = null) {
+    super({
+      statusCode: 401,
+      errorType: 'authentication',
+      customMessage: message,
+      field,
+      details
+    });
   }
+}
   
-  export default AuthenticationError;
+export default AuthenticationError;
   

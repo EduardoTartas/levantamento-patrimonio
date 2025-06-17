612e664860bbbedeb114293df060c7ff0e632ee4
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-17 17:40:23 -0400

    Aplicando permissões nos endpoints...

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ import usuarioController from '../controllers/UsuarioController.js';
 
 import { asyncWrapper } from '../utils/helpers/index.js';
 import AuthMiddleware from '../middlewares/AuthMiddleware.js';
+import AuthPermission from '../middlewares/AuthPermission.js';
 
 const router = express.Router();
 const UsuarioController = new usuarioController(); 
@@ -11,9 +12,9 @@ const UsuarioController = new usuarioController();
 router
     .get("/usuarios", /*AuthMiddleware, *authPermission,*/ asyncWrapper(UsuarioController.listar.bind(UsuarioController)))
     .get("/usuarios/:id", /*AuthMiddleware, authPermission,*/ asyncWrapper(UsuarioController.listar.bind(UsuarioController)))
-    .post("/usuarios", AuthMiddleware, /*authPermission,*/ asyncWrapper(UsuarioController.criar.bind(UsuarioController)))
-    .patch("/usuarios/:id", AuthMiddleware,/*authPermission,*/ asyncWrapper(UsuarioController.atualizar.bind(UsuarioController)))
-    .put("/usuarios/:id", AuthMiddleware,/*leware, authPermission,*/ asyncWrapper(UsuarioController.atualizar.bind(UsuarioController)))
-    .delete("/usuarios/:id", AuthMiddleware, /*AuthMiddPermission*/ asyncWrapper(UsuarioController.deletar.bind(UsuarioController)))
+    .post("/usuarios", /*AuthMiddleware,*/ AuthPermission, asyncWrapper(UsuarioController.criar.bind(UsuarioController)))
+    .patch("/usuarios/:id", /*AuthMiddleware,*/ AuthPermission, asyncWrapper(UsuarioController.atualizar.bind(UsuarioController)))
+    .put("/usuarios/:id",/*AuthMiddleware,*/ /*leware,*/ AuthPermission, asyncWrapper(UsuarioController.atualizar.bind(UsuarioController)))
+    .delete("/usuarios/:id", /*AuthMiddleware,*/ AuthPermission, asyncWrapper(UsuarioController.deletar.bind(UsuarioController)))
 
 export default router;

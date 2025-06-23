825f93628ec04e09e5919454e72f10fd4f9cf264
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-06-23 17:57:36 -0400

    refatora o codigo da geração de urls

--- src/services/LevantamentoService.js
+++ src/services/LevantamentoService.js
@@ -2,9 +2,9 @@ import LevantamentoRepository from "../repositories/LevantamentoRepository.js";
 import InventarioService from "./InventarioService.js";
 import SalaService from "./SalaService.js";
 import BemService from "./BemService.js";
-import { CustomError, HttpStatusCodes, messages } from "../utils/helpers/index.js";
-import minioClient from '../config/minioConnect.js';
-import 'dotenv/config';
+import {CustomError, HttpStatusCodes, messages} from "../utils/helpers/index.js";
+import minioClient from "../config/minioConnect.js";
+import "dotenv/config";
 
 class LevantamentoService {
     constructor() {
@@ -16,7 +16,13 @@ class LevantamentoService {
 
     async listar(req) {
         console.log("Estou no listar em LevantamentoService");
-        return this.repository.listar(req);
+        const resultado = await this.repository.listar(req);
+
+        if (resultado) {
+            await this.gerarUrlsAssinadas(resultado);
+        }
+        
+        return resultado;
     }
 
     async criar(parsedData) {
@@ -24,26 +30,26 @@ class LevantamentoService {
 
         await this.inventarioService.ensureInvExists(parsedData.inventario);
         await this.ensureLevantamentoUnico(parsedData.inventario, parsedData.bemId);
-        if(parsedData.salaNova){
+        if (parsedData.salaNova) {
             await this.salaService.ensureSalaExists(parsedData.salaNova);
         }
         const bem = await this.bemService.ensureBemExists(parsedData.bemId);
-        
-        const nomeResponsavel = bem.responsavel?.nome || '';
-        const cpfResponsavel = bem.responsavel?.cpf || '';
+
+        const nomeResponsavel = bem.responsavel?.nome || "";
+        const cpfResponsavel = bem.responsavel?.cpf || "";
 
         parsedData.bem = {
-            responsavel:{
+            responsavel: {
                 nome: nomeResponsavel,
-                cpf: cpfResponsavel
+                cpf: cpfResponsavel,
             },
             tombo: bem.tombo,
             nome: bem.nome,
             descricao: bem.descricao,
             salaId: bem.sala,
-            id: bem.id
+            id: bem.id,
         };
-       
+
         return this.repository.criar(parsedData);
     }
 
@@ -60,34 +66,35 @@ class LevantamentoService {
         await this.ensureLevantamentoExists(id);
         const levantamento = await this.ensureLevantamentoExists(id);
         await this.inventarioService.ensureInvExists(levantamento.inventario._id);
-        
-        const  deletado = await this.repository.deletar(id);
+
+        const deletado = await this.repository.deletar(id);
 
         deletado.imagem.forEach(async (fileName) => {
-                await this.deletarMinio(fileName);
+            await this.deletarMinio(fileName);
         });
 
         return deletado;
     }
-    
+
     async adicionarFoto(id, file) {
         console.log("Estou no adicionarFoto em LevantamentoService");
         const levantamento = await this.ensureLevantamentoExists(id);
-        
+
         const imagemInfo = await this.enviarMinio(file);
-         
+
         levantamento.imagem.push(imagemInfo.fileName);
-        
-        const resultado = await this.repository.atualizar(id, { imagem: levantamento.imagem });
-        
-        return resultado;
-        
+
+        const resultado = await this.repository.atualizar(id, {
+            imagem: levantamento.imagem,
+        });
+
+        return await this.gerarUrlsAssinadas(resultado);
     }
 
     async deletarFoto(id) {
         console.log("Estou no deletarFoto em LevantamentoService");
         const levantamento = await this.ensureLevantamentoExists(id);
-         
+
         if (levantamento.imagem.length > 0) {
             levantamento.imagem.forEach(async (fileName) => {
                 await this.deletarMinio(fileName);
@@ -99,34 +106,39 @@ class LevantamentoService {
                 customMessage: "Nenhuma imagem encontrada no levantamento",
             });
         }
-        
-        const resultado = await this.repository.atualizar(id, { imagem: levantamento.imagem });
-        
+
+        const resultado = await this.repository.atualizar(id, {
+            imagem: levantamento.imagem,
+        });
+
         return resultado;
     }
-    
+
     // --- MÉTODOS AUXILIARES ---
-    
+
     async enviarMinio(file) {
         const bucket = process.env.MINIO_BUCKET_FOTOS;
-        
         const targetName = `${Date.now()}-${file.originalname}`;
-        
+
+        const metaData = {
+            "Content-Type": file.mimetype,
+        };
+
         const uploaded = await minioClient.putObject(
             bucket,
             targetName,
             file.buffer,
             file.size,
-            file.mimetype
+            metaData
         );
-        
-         if (!uploaded){
-             throw new CustomError({
+
+        if (!uploaded) {
+            throw new CustomError({
                 statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR.code,
                 customMessage: `Erro ao enviar arquivo: ${error.message}`,
             });
         }
-        
+
         return {
             bucket,
             fileName: targetName,
@@ -135,10 +147,10 @@ class LevantamentoService {
             mimetype: file.mimetype,
         };
     }
-    
+
     async deletarMinio(fileName) {
         const bucket = process.env.MINIO_BUCKET_FOTOS;
-        
+
         //necessario o uso do try/catch para tratar erros de remoção
         try {
             await minioClient.removeObject(bucket, fileName);
@@ -150,7 +162,27 @@ class LevantamentoService {
             });
         }
     }
-    
+
+    async gerarUrlsAssinadas(doc) {
+        if (!doc) return null;
+
+        doc.imagemUrls = [];
+
+        if (doc.imagem && doc.imagem.length > 0) {
+            //necessario o uso do try/catch para tratar erros de geração de URLs
+            try {
+                const bucketName = process.env.MINIO_BUCKET_FOTOS;
+                const promises = doc.imagem.map(fileName =>
+                    minioClient.presignedGetObject(bucketName, fileName, 3600)
+                );
+                doc.imagemUrls = await Promise.all(promises);
+            } catch (error) {
+                console.error(`Falha ao gerar URL para o documento ${doc._id}:`, error);
+            }
+        }
+        return doc;
+    }
+
     async ensureLevantamentoExists(id) {
         const levantamentoExistente = await this.repository.buscarPorId(id);
         if (!levantamentoExistente) {
@@ -161,13 +193,17 @@ class LevantamentoService {
         }
         return levantamentoExistente;
     }
-    
+
     async ensureLevantamentoUnico(inventarioId, bemId) {
-        const levantamentoExistente = await this.repository.buscarPorInventarioEBem(inventarioId, bemId);
+        const levantamentoExistente = await this.repository.buscarPorInventarioEBem(
+            inventarioId,
+            bemId
+        );
         if (levantamentoExistente) {
-             throw new CustomError({
+            throw new CustomError({
                 statusCode: HttpStatusCodes.BAD_REQUEST.code,
-                customMessage: "Já existe um levantamento para este bem neste inventário.",
+                customMessage:
+                    "Já existe um levantamento para este bem neste inventário.",
             });
         }
     }

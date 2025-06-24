2bc7b7598ff6b07be0114ceb0df4d9ba8221949e
ThiagoHS <thiagohenssuchi@gmail.com>
2025-06-24 18:11:34 -0400

    Rota relatorios implementada, falta integração de permissões e validações...

--- src/routes/relatorioRoutes.js
+++ src/routes/relatorioRoutes.js
@@ -1,15 +1,14 @@
 import express from "express";
-//import RelatorioController from "../controllers/RelatorioController.js";
-//import {asyncWrapper} from '../utils/helpers/index.js';
+import RelatorioController from "../controllers/RelatorioController.js";
+import {asyncWrapper} from '../utils/helpers/index.js';
 //import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 //import authPermission from '../middlewares/AuthPermission.js';
 
 const router = express.Router();
 
-//const relatorioController = new RelatorioController();
+const relatorioController = new RelatorioController();
 
-router
 // Espera os parâmetros de query: inventarioId (obrigatório), sala (opcional), tipoRelatorio (obrigatório)
-.get("/relatorios",/*AuthMiddleware,authPermission,asyncWrapper(relatorioController.gerarRelatorio.bind(relatorioController))*/);
+router.get("/relatorios",/*AuthMiddleware,authPermission,*/ asyncWrapper(relatorioController.gerar.bind(relatorioController)));
 
 export default router;
